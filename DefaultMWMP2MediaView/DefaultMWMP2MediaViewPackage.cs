a570a37c445a0d0edd6e49edc5f063d7ee23f2c6
Colliot Vincent <colliotv.epitech@gmail.com>
2015-12-03 19:15:18 +0000

    Add pckge media

---
+++ DefaultMWMP2MediaView/DefaultMWMP2MediaViewPackage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using SharedProperties.Interfaces;
+
+namespace DefaultMWMP2MediaView
+{
+    // ReSharper disable once InconsistentNaming
+    [Export(typeof(IMediaViewerPackage))]
+    public class DefaultMWMP2MediaViewPackage : IMediaViewerPackage
+    {
+        #region IMediaViewerPackage Implementation
+
+        public string MediaViewerPackageName { get; }
+
+        public IMediaViewer GetMediaViewerForFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+        public DefaultMWMP2MediaViewPackage()
+        {
+            MediaViewerPackageName = "DefaultMWMP2mediaview";
+        }
+    }
+}
